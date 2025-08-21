f28cef501ef3a49a91b1baed2255f04792263ee1
tovalh <cristobalva16@gmail.com>
2025-08-21 13:58:39 -0400

    Force HTTPS to all routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+            \URL::forceScheme('https');
+
+            $this->app['url']->forceRootUrl('https://cliento.cl');
+            //
+        }
     }
 }
